c661b1de16940ef4a902d7fd52386d2ca3035388
Dan Marshall <daniel.o.marshall@gmail.com>
2021-11-11 15:40:10 +0000

    built search functionality

--- controllers/posts.js
+++ controllers/posts.js
@@ -208,11 +208,129 @@ var PostsController = {
       });
   },
 
-  // New: function (req, res) {
-  //   const resParams = { title: 'New Post', user: req.session.user };
+  Search: function (req, res) {
+    const search = req.query.search;
 
-  //   res.render('posts/new', resParams);
-  // },
+    Post.aggregate([
+      {
+        // Get poster for their name
+        $lookup: {
+          from: User.collection.name,
+          localField: 'poster',
+          foreignField: 'email',
+          as: 'posterName'
+        }
+      },
+      {
+        // Get comments linked to post
+        $lookup: {
+          from: Comment.collection.name,
+          localField: '_id',
+          foreignField: 'postId',
+          as: 'comments'
+        }
+      },
+      {
+        $unwind: {
+          path: '$comments',
+          preserveNullAndEmptyArrays: true
+        }
+      },
+      {
+        $lookup: {
+          from: User.collection.name,
+          localField: 'comments.poster',
+          foreignField: 'email',
+          as: 'comments.commenterInfo'
+        }
+      },
+      {
+        $sort: { 'comments.createdAt': -1 }
+      },
+      {
+        $group: {
+          _id: '$_id',
+          message: { $first: '$message' },
+          poster: { $first: '$poster' },
+          posterName: { $first: '$posterName' },
+          updatedAt: { $first: '$updatedAt' },
+          createdAt: { $first: '$createdAt' },
+          likes: { $first: '$likes' },
+          comments: { $push: '$comments' },
+          imageLink: { $first: '$imageLink' }
+        }
+      },
+      {
+        $project: {
+          _id: 1,
+          message: 1,
+          poster: 1,
+          posterName: 1,
+          updatedAt: 1,
+          createdAt: 1,
+          likes: 1,
+          imageLink: 1,
+          comments: {
+            $filter: {
+              input: '$comments',
+              as: 'c',
+              cond: { $ifNull: ['$$c._id', false] }
+            }
+          }
+        }
+      }
+    ])
+      .sort({ createdAt: 'desc' })
+      .exec(function (err, aggregateRes) {
+        if (err) {
+          throw err;
+        } else {
+          let filteredPosts = aggregateRes.filter(post =>
+            post.message.includes(search)
+          );
+          let formattedPosts = filteredPosts.map(post => {
+            let date = new Date(post.createdAt);
+            post.dateString = timeDifference(date);
+
+            post.comments = post.comments.sort((a, b) => {
+              return new Date(b.createdAt) - new Date(a.createdAt);
+            });
+            post.comments.forEach(comment => {
+              let date = new Date(comment.createdAt);
+              comment.dateString = timeDifference(date);
+              comment.toCollapse = post.comments.indexOf(comment) > 1;
+              comment.commentLiked = comment.likes.includes(
+                req.session.user.email
+              );
+              comment.commentLikes = comment.likes.length;
+              if (comment.commenterInfo.length < 1) {
+                comment.commenterName = 'Anonymous';
+              } else {
+                comment.commenterName = comment.commenterInfo[0].name;
+              }
+            });
+            post.needsCommentExpander = post.comments.length > 2;
+
+            post.likes = post.likes === undefined ? [] : post.likes;
+            return {
+              ...post,
+              posterName: post.posterName[0]
+                ? post.posterName[0].name
+                : 'Anonymous',
+              postLikes: post.likes.length,
+              postLiked: post.likes.includes(req.session.user.email)
+            };
+          });
+          const resParams = {
+            posts: formattedPosts,
+            title: 'Posts',
+            user: req.session.user
+          };
+
+          res.render('posts/search', resParams);
+        }
+      });
+  },
 
   Create: function (req, res) {
     // console.log(req);
